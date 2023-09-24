ae0c7b9f0cfb39ccb30c6468b768345d07d14f22
Šarūnas <52723739+slekstut@users.noreply.github.com>
2023-09-24 22:20:50 +0300

    add mobile fix

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -6,6 +6,7 @@
                 <p>Esame patyrusių inžinierių ir architektų komanda, kuri nenuilstamai dirba, kad pasiūlytume Jums
                     aukščiausios klasės, pažangiausius pramoninius pastatus. Mūsų atsidavimas naujovėms ir kompetencijai
                     suteikė mums metalo konstrukcijų pramonės lyderių reputaciją.</p>
+                    <p>Mūsų metalinės konstrukcijos užtikrina aukštą našumą ir pritaikomumą, suteikdamos idealią aplinką jūsų pramoninei veiklai. Mūsų konstrukcijos sukurtos taip, kad atlaikytų sudėtingiausias sąlygas, užtikrindamos ilgalaikę investicijų grąžą.</p>
             </div>
         </div>
         <div class="intro technology" data-aos="fade-in">
@@ -14,7 +15,7 @@
                 <div class="card">
                     <div class="card__header">
                         <div class="icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24">
+                            <svg viewBox="0 0 24 24">
                                 <path fill="#ffffff" d="m24,2v3H0v-3h24ZM7,22h17v-3H7v3Zm0-9h17v-3H7v3ZM0,22h3v-12H0v12Z" />
                             </svg>
                         </div>
@@ -23,7 +24,8 @@
                     <div class="card__content">
                         <p>Gamybos proceso
                             automatizavimas
-                            ir itin aukšta įrangos kokybė leidžia išlaikyti vos 1 mm karkasų geometrijos paklaidą. Pastatų iš
+                            ir itin aukšta įrangos kokybė leidžia išlaikyti vos 1 mm karkasų geometrijos paklaidą. Pastatų
+                            iš
                             lengvų plieno konstrukcijų parametrai – itin tikslūs. Tokios konstrukcijos apsaugotos
                             nuo
                             biologinio drėgmės poveikio ir temperatūrų svyravimo. Dėl šių charakteristikų pastatai gali būti
@@ -36,9 +38,35 @@
                 <div class="card">
                     <div class="card__header">
                         <div class="icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24">
-                                <path fill="#ffffff" d="m24,2v3H0v-3h24ZM7,22h17v-3H7v3Zm0-9h17v-3H7v3ZM0,22h3v-12H0v12Z" />
+                            <svg viewBox="0 0 24 24">
+                                <path fill="#ffffff"
+                                    d="M23.5,24H4.5c-2.48,0-4.5-2.02-4.5-4.5V.5C0,.22,.22,0,.5,0s.5,.22,.5,.5V19.5c0,1.93,1.57,3.5,3.5,3.5H23.5c.28,0,.5,.22,.5,.5s-.22,.5-.5,.5Zm.5-6.5c0-.28-.22-.5-.5-.5h-6c-.28,0-.5,.22-.5,.5s.22,.5,.5,.5h6c.28,0,.5-.22,.5-.5Zm-5-6c0-.28-.22-.5-.5-.5H9.5c-.28,0-.5,.22-.5,.5s.22,.5,.5,.5h9c.28,0,.5-.22,.5-.5Zm-7-6c0-.28-.22-.5-.5-.5H5.5c-.28,0-.5,.22-.5,.5s.22,.5,.5,.5h6c.28,0,.5-.22,.5-.5Z" />
                             </svg>
+
+                        </div>
+                        <div class="card__title">Sluoksninės plokštės</div>
+
+                    </div>
+                    <div class="card__content">
+                        <p>Pastatų sienų ir stogų apdailos medžiaga, sudaryta iš šilumą
+                            izoliuojančio
+                            užpildo ir skardos. Tai ne konstrukcinė, o dengiamoji medžiaga. Konstrukcinę funkciją atlieka ne
+                            pačios
+                            plokštės, o plieninis karkasas, prie kurio jos tvirtinamos.
+
+                        </p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card__header">
+                        <div class="icon">
+                            <svg viewBox="0 0 24 24" width="512" height="512">
+                                <path
+                                fill="#ffffff"
+                                    d="M23.5,24H.5c-.28,0-.5-.22-.5-.5s.22-.5,.5-.5H23.5c.28,0,.5,.22,.5,.5s-.22,.5-.5,.5ZM1,0C.45,0,0,.45,0,1s.45,1,1,1,1-.45,1-1S1.55,0,1,0Zm0,5C.45,5,0,5.45,0,6s.45,1,1,1,1-.45,1-1-.45-1-1-1Zm22,0c-.55,0-1,.45-1,1s.45,1,1,1,1-.45,1-1-.45-1-1-1ZM1,17C.45,17,0,17.45,0,18s.45,1,1,1,1-.45,1-1-.45-1-1-1Zm22,0c-.55,0-1,.45-1,1s.45,1,1,1,1-.45,1-1-.45-1-1-1ZM23,0c-.55,0-1,.45-1,1s.45,1,1,1,1-.45,1-1-.45-1-1-1Zm-5,0c-.55,0-1,.45-1,1s.45,1,1,1,1-.45,1-1-.45-1-1-1Zm-6,0c-.55,0-1,.45-1,1s.45,1,1,1,1-.45,1-1-.45-1-1-1ZM6,0c-.55,0-1,.45-1,1s.45,1,1,1,1-.45,1-1-.45-1-1-1ZM1,11C.45,11,0,11.45,0,12s.45,1,1,1,1-.45,1-1-.45-1-1-1Zm22,0c-.55,0-1,.45-1,1s.45,1,1,1,1-.45,1-1-.45-1-1-1Zm-5,0c-.55,0-1,.45-1,1s.45,1,1,1,1-.45,1-1-.45-1-1-1Zm-6,0c-.55,0-1,.45-1,1s.45,1,1,1,1-.45,1-1-.45-1-1-1Zm-6,0c-.55,0-1,.45-1,1s.45,1,1,1,1-.45,1-1-.45-1-1-1Zm7,7c0-.55-.45-1-1-1s-1,.45-1,1,.45,1,1,1,1-.45,1-1Zm0-12c0-.55-.45-1-1-1s-1,.45-1,1,.45,1,1,1,1-.45,1-1Z" />
+                            </svg>
+
                         </div>
                         <div class="card__title">Sandwich daugiasluoksnės plokštės</div>
 
@@ -50,27 +78,6 @@
                             putų polistireno, mineralinės vatos ir poliuretano (poliizocianurato – PIR) užpildu.
 
                             Daugiausia skiriasi jų šilumos ir garso izoliacinės savybės, atsparumas ugniai, svoris.</p>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <div class="card__header">
-                        <div class="icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24">
-                                <path fill="#ffffff" d="m24,2v3H0v-3h24ZM7,22h17v-3H7v3Zm0-9h17v-3H7v3ZM0,22h3v-12H0v12Z" />
-                            </svg>
-                        </div>
-                        <div class="card__title">Sluoksninė plokštė</div>
-
-                    </div>
-                    <div class="card__content">
-                        <p>Pastatų sienų ir stogų apdailos medžiaga, sudaryta iš šilumą
-                            izoliuojančio
-                            užpildo ir skardos. Tai ne konstrukcinė, o dengiamoji medžiaga. Konstrukcinę funkciją atlieka ne
-                            pačios
-                            plokštės, o plieninis karkasas, prie kurio jos tvirtinamos.
-
-                        </p>
                     </div>
                 </div>
             </div>
@@ -92,13 +99,16 @@
 
 .intro .intro__content {
     display: flex;
-    flex-wrap: wrap;
     gap: 24px;
     margin-top: 48px;
     font-size: 18px;
     font-weight: 400;
     line-height: 30px;
     color: var(--c-white);
+}
+
+.intro .intro__content p {
+    flex: 1 1 50%;
 }
 
 intro.technology {
@@ -117,6 +127,7 @@ intro.technology {
     justify-content: center;
     align-items: center;
     gap: 24px;
+    min-height: 96px;
 }
 
 .card .card__title {
@@ -135,6 +146,13 @@ intro.technology {
     padding: 24px;
     width: calc(33.333% - 24px);
     background-color: var(--c-black-mute);
+    transition: all 0.3s ease-in-out;
+}
+
+.card:hover {
+    background-color: var(--c-black-soft);
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+
 }
 
 .intro.technology .card .icon {
@@ -144,6 +162,11 @@ intro.technology {
     display: flex;
     justify-content: center;
     align-items: center;
+}
+
+.intro.technology .card .icon svg {
+    width: 48px;
+    height: 48px;
 }
 
 .intro .intro__content p {
@@ -167,4 +190,8 @@ intro.technology {
         flex-direction: column;
         gap: 24px;
     }
+    .card {
+        width: 100%;
+    }
+    
 }</style>
