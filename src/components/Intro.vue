cbefa4bba29ca15d06a9b3349da4b4ba0d92b2f7
Šarūnas <52723739+slekstut@users.noreply.github.com>
2023-09-24 22:03:58 +0300

    add cards

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -7,23 +7,72 @@
                     aukščiausios klasės, pažangiausius pramoninius pastatus. Mūsų atsidavimas naujovėms ir kompetencijai
                     suteikė mums metalo konstrukcijų pramonės lyderių reputaciją.</p>
             </div>
-            <h3 class="subtitle technology">Naudojamos technologijos</h3>
+        </div>
+        <div class="intro technology" data-aos="fade-in">
+            <h3 class="subtitle">Naudojamos technologijų privalumai</h3>
             <div class="intro__content">
-                <p>Lengvųjų plieno konstrukcijų technologija (LPKT) – tai pirmaujanti statybos technologija, grindžiama
-                    naujausiais taikytinais statybų srityje mokslo ir technikos atradimais. Gamybos proceso automatizavimas
-                    ir itin aukšta įrangos kokybė leidžia išlaikyti vos 1 mm karkasų geometrijos paklaidą.</p>
-                <p>Pastatų iš lengvų plieno konstrukcijų parametrai – itin tikslūs. Tokios konstrukcijos apsaugotos nuo
-                    biologinio drėgmės poveikio ir temperatūrų svyravimo. Dėl šių charakteristikų pastatai gali būti
-                    eksploatuojami itin ilgą laiką, o jų remontas kainuoja mažiau nei įprasto tradicinio pastato, kas
-                    įtakoja tokių pastatų ekonominį pagrįstumą.</p>
-                <p>Sluoksninė plokštė – tai pastatų sienų ir stogų apdailos medžiaga, sudaryta iš šilumą izoliuojančio
-                    užpildo ir skardos. Tai ne konstrukcinė, o dengiamoji medžiaga. Konstrukcinę funkciją atlieka ne pačios
-                    plokštės, o plieninis karkasas, prie kurio jos tvirtinamos.
+                <div class="card">
+                    <div class="card__header">
+                        <div class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24">
+                                <path fill="#ffffff" d="m24,2v3H0v-3h24ZM7,22h17v-3H7v3Zm0-9h17v-3H7v3ZM0,22h3v-12H0v12Z" />
+                            </svg>
+                        </div>
+                        <div class="card__title">Lengvųjų plieno konstrukcijų technologija (LPKT)</div>
+                    </div>
+                    <div class="card__content">
+                        <p>Gamybos proceso
+                            automatizavimas
+                            ir itin aukšta įrangos kokybė leidžia išlaikyti vos 1 mm karkasų geometrijos paklaidą. Pastatų iš
+                            lengvų plieno konstrukcijų parametrai – itin tikslūs. Tokios konstrukcijos apsaugotos
+                            nuo
+                            biologinio drėgmės poveikio ir temperatūrų svyravimo. Dėl šių charakteristikų pastatai gali būti
+                            eksploatuojami itin ilgą laiką, o jų remontas kainuoja mažiau nei įprasto tradicinio pastato,
+                            kas
+                            įtakoja tokių pastatų ekonominį pagrįstumą.</p>
+                    </div>
+                </div>
 
-                    Sdaugiasluoksnės plokštės klasifikuojamos pagal izoliacinės medžiagos rūšį. Prekyboje yra plokščių su
-                    putų polistireno, mineralinės vatos ir poliuretano (poliizocianurato – PIR) užpildu.
+                <div class="card">
+                    <div class="card__header">
+                        <div class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24">
+                                <path fill="#ffffff" d="m24,2v3H0v-3h24ZM7,22h17v-3H7v3Zm0-9h17v-3H7v3ZM0,22h3v-12H0v12Z" />
+                            </svg>
+                        </div>
+                        <div class="card__title">Sandwich daugiasluoksnės plokštės</div>
 
-                    Daugiausia skiriasi jų šilumos ir garso izoliacinės savybės, atsparumas ugniai, svoris.</p>
+                    </div>
+                    <div class="card__content">
+                        <p>Klasifikuojamos pagal izoliacinės medžiagos rūšį. Prekyboje yra
+                            plokščių
+                            su
+                            putų polistireno, mineralinės vatos ir poliuretano (poliizocianurato – PIR) užpildu.
+
+                            Daugiausia skiriasi jų šilumos ir garso izoliacinės savybės, atsparumas ugniai, svoris.</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card__header">
+                        <div class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24">
+                                <path fill="#ffffff" d="m24,2v3H0v-3h24ZM7,22h17v-3H7v3Zm0-9h17v-3H7v3ZM0,22h3v-12H0v12Z" />
+                            </svg>
+                        </div>
+                        <div class="card__title">Sluoksninė plokštė</div>
+
+                    </div>
+                    <div class="card__content">
+                        <p>Pastatų sienų ir stogų apdailos medžiaga, sudaryta iš šilumą
+                            izoliuojančio
+                            užpildo ir skardos. Tai ne konstrukcinė, o dengiamoji medžiaga. Konstrukcinę funkciją atlieka ne
+                            pačios
+                            plokštės, o plieninis karkasas, prie kurio jos tvirtinamos.
+
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
@@ -52,8 +101,49 @@
     color: var(--c-white);
 }
 
-.intro .subtitle.technology {
+intro.technology {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+}
+
+.intro.technology .subtitle {
     margin-top: 64px;
+    max-width: 550px;
+}
+
+.card .card__header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 24px;
+}
+
+.card .card__title {
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 30px;
+    text-transform: uppercase;
+    color: var(--c-white);
+}
+
+.card__content {
+    margin-top: 12px;
+}
+
+.card {
+    padding: 24px;
+    width: calc(33.333% - 24px);
+    background-color: var(--c-black-mute);
+}
+
+.intro.technology .card .icon {
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
 .intro .intro__content p {
